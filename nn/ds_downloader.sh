68c334f8fcb673e4b27c580bf8c45c9bd8730a41
Elias Khsheibun <elias.khsheibun@netlight.com>
2019-02-16 10:52:18 +0100

    updated downloader to use the new data sets

--- nn/ds_downloader.sh
+++ nn/ds_downloader.sh
@@ -4,6 +4,9 @@ printf "*                                  *\n"
 printf "*    Attention. Achtung. 1.3 Gb    *\n"
 printf "*                                  *\n"
 printf "************************************\n"
-curl http://ec2-3-208-115-48.compute-1.amazonaws.com/ds.v001.tar.gz -O
-tar -zxvf ds.v001.tar.gz
-rm ds.v001.tar.gz
+curl http://ec2-3-208-115-48.compute-1.amazonaws.com/sim_camera_ds.v002.tar.gz -O
+curl http://ec2-3-208-115-48.compute-1.amazonaws.com/real_camera_ds.v001.tar.gz -O
+tar -zxvf sim_camera_ds.v002.tar.gz
+tar -zxvf real_camera_ds.v001.tar.gz
+rm real_camera_ds.v001.tar.gz
+rm sim_camera_ds.v002.tar.gz
