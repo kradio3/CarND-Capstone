36cc46eb0a5f9218d747edb2362f230eeeb80cea
nathan <nathansherrit@gmail.com>
2019-02-17 21:52:41 -0800

    minor clean up

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -30,13 +30,6 @@ class TLDetector(object):
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
-        '''
-        /vehicle/traffic_lights provides you with the location of the traffic light in 3D map space and
-        helps you acquire an accurate ground truth data source for the traffic light
-        classifier by sending the current color state of all traffic lights in the
-        simulator. When testing on the vehicle, the color state will not be available. You'll need to
-        rely on the position of the light and the camera image to predict it.
-        '''
         sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
         sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
 
@@ -58,15 +51,16 @@ class TLDetector(object):
 
     def pose_cb(self, msg):
         self.pose = msg
-	car_position = self.get_closest_waypoint(self.pose.pose)
-	
-	light_wp, state = self.process_traffic_lights()
 
+	# TODO move to image_cb 
+	# Get the light waypoint and state
+	light_wp, state = self.process_traffic_lights()
+	# If yellow or red publish waypoint
+	# Else publish -1
 	if state == 0 or state == 1:
 		self.upcoming_red_light_pub.publish(Int32(light_wp))
 	else:
 		self.upcoming_red_light_pub.publish(Int32(-1))
-
 
     def waypoints_cb(self, waypoints):
 	self.waypoints = waypoints
